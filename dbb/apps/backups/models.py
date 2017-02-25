0c0460f8efa7d5c6e80177a942dad1fda3fb0bd4
Juan Ignacio Sánchez <jiss2891@gmail.com>
2017-02-25 04:38:36 -0300

    add backup_file en modelo Backup

--- dbb/apps/backups/models.py
+++ dbb/apps/backups/models.py
@@ -1,11 +1,26 @@
 from __future__ import unicode_literals
 
+from datetime import datetime as dt
+from tiger import tiger
+
+from django.dispatch import receiver
 from django.db import models
+from django.db.models.signals import pre_save
+
 from dbb.apps.databases.models import Database
+
+
+def calculate_path(instance, filename):
+    hex_code = tiger(instance.backup_file.open().read()).hexdigest()
+    return 'backups_storage/{}/{}/{}.sql'.format(instance.db.name,
+            dt.strftime(dt.now(), "%d_%b_%Y"), hex_code)
+
 
 # Create your models here.
 class Backup(models.Model):
     db = models.ForeignKey(Database, verbose_name="Database",
             related_name='backups')
     date = models.DateTimeField(auto_now_add=True, verbose_name="Date")
-    # user ??
+    backup_file = models.FileField(upload_to=calculate_path, null=True,
+            blank=True)
+    #1user ??
