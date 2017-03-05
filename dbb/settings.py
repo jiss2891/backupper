81bb7a00f93f7774b41a67646f1919148069cb05
Juan Ignacio Sánchez <jiss2891@gmail.com>
2017-03-05 01:55:38 -0300

    add staticfiles configs and files

--- dbb/settings.py
+++ dbb/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MEDIA_ROOT = BASE_DIR + "/media"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 # Quick-start development settings - unsuitable for production
@@ -115,7 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = 'dbb/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'dbb', 'static')
+]
+
+STATIC_URL = "/static/"
 
 try:
     from local_settings import *
