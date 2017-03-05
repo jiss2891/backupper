17d28695c8ded4eb611b0a7fc97170cd79a7fc81
Juan Ignacio Sánchez <jiss2891@gmail.com>
2017-03-05 02:29:28 -0300

    add index template and fix base template

--- dbb/urls.py
+++ dbb/urls.py
@@ -18,6 +18,6 @@ from django.views.generic import TemplateView
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', TemplateView.as_view(template_name="base.html")),
+    url(r'^', TemplateView.as_view(template_name="index.html"), name='index'),
     url(r'^admin/', admin.site.urls),
 ]
