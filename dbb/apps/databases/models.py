ebe1f7fbf6bbfeeb996050da80525ed84897fc8e
Juan Ignacio Sánchez <jiss2891@gmail.com>
2017-03-11 03:06:47 -0300

    add creacion pgpass y funcion pgdump

--- dbb/apps/databases/models.py
+++ dbb/apps/databases/models.py
@@ -1,6 +1,10 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import os, getpass # para detectar al usuario
+
 from django.db import models
+from django.conf import settings
 
 
 class Database(models.Model):
@@ -9,6 +13,7 @@ class Database(models.Model):
 
 class RemoteDatabase(Database):
     host = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
     port = models.IntegerField()
 
@@ -27,5 +32,17 @@ class MysqlBackend(RemoteDatabase):
 
 class PsqlBackend(RemoteDatabase):
 
+    def save(self, *args, **kwargs):
+        # añado al archivo ~/.pgpass la contraseña nueva.
+        usuario = getpass.getuser()
+        print usuario
+        pgpass = open(os.path.join('/home', usuario, '.pgpass'), 'a+')
+        pgpass.write(
+            ':'.join([self.host, str(self.port), self.name, self.username, self.password])[:-1] + "\n"
+        )
+        pgpass.close()
+        super(PsqlBackend, self).save(args, kwargs)
+
+
     def get_dump(self):
-        os.system("pgdump -d {} -u {} ")
+        os.system("pgdump -U {} -d {} -h {} -p {}".format(self.username, self.name, self.host, self.port))
