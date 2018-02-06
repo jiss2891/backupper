c5351ba3c2c2f6a63907919f4247ddef07eb6cb0
Juan Ignacio Sánchez <jiss2891@gmail.com>
2018-02-06 04:05:15 -0300

    add dump code for mysql backend

--- dbb/apps/databases/models.py
+++ dbb/apps/databases/models.py
@@ -34,8 +34,44 @@ class SqliteBackend(Database):
 
 class MysqlBackend(RemoteDatabase):
 
+    login_path = models.CharField(max_length=100, default="__badfood__")
+
+    def save(self, *args, **kwargs):
+        # añado al archivo ~/.pgpass la contraseña nueva.
+        usuario = getpass.getuser()
+        sanitize_control = sanitize.bash_sanitize(
+            [
+                self.host,
+                self.port,
+                self.username,
+                self.name
+            ]
+        )
+        if sanitize_control:
+            # genero el archivo, si no existe
+            os.system("touch ~/.my.cnf")
+            # corrijo los permisos
+            os.system("chmod 600 ~/.my.cnf")
+            # abro archivo con permisos de append
+            mylogin = open(os.path.join('/home', usuario, '.mylogin.cnf'), 'a+')
+            #pg_line = unicode(self.host) + ":" + self.port + ":" + unicode(self.name) + ":" + self.username + ":" + self.password
+            # genero el nombre de la instancia
+            self.login_path = "{}".format(self.name)
+            mylogin.write("[client{}]\n".format(self.login_path))
+            mylogin.write("user={}\n".format(self.username))
+            mylogin.write("password={}\n".format(self.password))
+            mylogin.write("host={}\n".format(self.host))
+            mylogin.write("port={}\n".format(self.port))
+            mylogin.close()
+            super(MysqlBackend, self).save(args, kwargs)
+        else:
+            raise ValueError("Cadenas ingresadas inválidas.")
+
     def get_dump(self):
-        return "No implementado"
+        command = "mysqldump --defaults-group-suffix={} -u {} -h {} -P {} {}".format(self.login_path, self.username, self.host, self.port,
+            self.name)
+        print command
+        return os.popen(command)
 
 
 class PsqlBackend(RemoteDatabase):
