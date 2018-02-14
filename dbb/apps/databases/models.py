6df45f9da4d60a8be5a869343ea20977da019c00
Juan Ignacio Sánchez <jiss2891@gmail.com>
2018-02-14 01:29:09 -0300

    fix several unicode issues + sanitize control

--- dbb/apps/databases/models.py
+++ dbb/apps/databases/models.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import os, getpass # para detectar al usuario
+import os, codecs, getpass # para detectar al usuario
 
 from django.db import models
 from django.conf import settings
 
-from dbb.utils import sanitize
+from dbb.utils import sanitize, files
 
 
 class Database(models.Model):
@@ -52,7 +52,7 @@ class MysqlBackend(RemoteDatabase):
     def save(self, *args, **kwargs):
         # añado al archivo ~/.pgpass la contraseña nueva.
         usuario = getpass.getuser()
-        sanitize_control = sanitize.bash_sanitize(
+        sanitize_status, matches = sanitize.bash_sanitize(
             [
                 self.host,
                 self.port,
@@ -60,28 +60,22 @@ class MysqlBackend(RemoteDatabase):
                 self.name
             ]
         )
-        if sanitize_control:
-            # genero el archivo, si no existe
-            os.system("touch ~/.my.cnf")
-            # corrijo los permisos
-            os.system("chmod 600 ~/.my.cnf")
-            # abro archivo con permisos de append
-            mylogin = open(os.path.join('/home', usuario, '.my.cnf'), 'a+')
-            #pg_line = unicode(self.host) + ":" + self.port + ":" + unicode(self.name) + ":" + self.username + ":" + self.password
+        if sanitize_status:
+            mylogin = files.open_access_table('mysql', usuario)
             # genero el nombre de la instancia
-            self.login_path = "{}".format(self.name)
-            mylogin.write("[client{}]\n".format(self.login_path))
-            mylogin.write("user={}\n".format(self.username))
-            mylogin.write("password={}\n".format(self.password))
-            mylogin.write("host={}\n".format(self.host))
-            mylogin.write("port={}\n".format(self.port))
+            self.login_path = u"{}".format(self.name)
+            mylogin.write(u"[client{}]\n".format(self.login_path))
+            mylogin.write(u"user={}\n".format(self.username))
+            mylogin.write(u"password={}\n".format(self.password))
+            mylogin.write(u"host={}\n".format(self.host))
+            mylogin.write(u"port={}\n".format(self.port))
             mylogin.close()
             super(MysqlBackend, self).save(args, kwargs)
         else:
-            raise ValueError("Cadenas ingresadas inválidas.")
+            raise ValueError("Cadenas ingresadas inválidas: {}".format(matches))
 
     def get_dump(self):
-        command = "mysqldump --defaults-group-suffix={} -u {} -h {} -P {} {}".format(self.login_path, self.username, self.host, self.port,
+        command = u"mysqldump --defaults-group-suffix={} -u {} -h {} -P {} {}".format(self.login_path, self.username, self.host, self.port,
             self.name)
         print command
         return os.popen(command)
@@ -95,7 +89,7 @@ class PsqlBackend(RemoteDatabase):
     def save(self, *args, **kwargs):
         # añado al archivo ~/.pgpass la contraseña nueva.
         usuario = getpass.getuser()
-        sanitize_control = sanitize.bash_sanitize(
+        sanitize_status, matches = sanitize.bash_sanitize(
             [
                 self.host,
                 self.port,
@@ -103,23 +97,18 @@ class PsqlBackend(RemoteDatabase):
                 self.name
             ]
         )
-        if sanitize_control:
-            # genero el archivo, si no existe
-            os.system("touch ~/.pgpass")
-            # corrijo los permisos
-            os.system("chmod 600 ~/.pgpass")
-            # abro archivo con permisos de append
-            pgpass = open(os.path.join('/home', usuario, '.pgpass'), 'a+')
-            pg_line = "{}:{}:{}:{}:{}\n".format(self.host, self.port, self.name, self.username, self.password)
+        if sanitize_status:
+            pgpass = files.open_access_table('psql', usuario)
+            pg_line = u"{}:{}:{}:{}:{}\n".format(self.host, self.port, self.name, self.username, self.password)
             pgpass.write(pg_line)
             pgpass.close()
             super(PsqlBackend, self).save(args, kwargs)
         else:
-            raise ValueError("Cadenas ingresadas inválidas.")
+            raise ValueError("Cadenas ingresadas inválidas: {}".format(matches))
 
 
     def get_dump(self):
         """
         retorna un popen sin leer
         """
-        return os.popen("pg_dump -h {} -p {} -U {} -d {} ".format(self.host, self.port, self.username, self.name))
+        return os.popen(u"pg_dump -h {} -p {} -U {} -d {} ".format(self.host, self.port, self.username, self.name))
--- dbb/utils/sanitize.py
+++ dbb/utils/sanitize.py
@@ -8,11 +8,23 @@ def bash_sanitize(in_str, extra_excluded=None):
         in_str = [in_str]
 
     # mezclo todas las cadenas
-    in_str = ("{}" * len(in_str)).format(*in_str)
+    container_string = u"{}" * len(in_str)
+    in_str = container_string.format(*in_str)
 
     # defino los caracteres que estan prohibidos
     excluded_chars = [';', '<', '>', '|']
     if extra_excluded:
         excluded_chars.extend(extra_excluded)
 
-    return not (True in map(lambda x: x in excluded_chars, in_str))
+    matches = []
+    for exc in excluded_chars:
+        if exc in in_str:
+            matches.append(exc)
+
+
+    import ipdb; ipdb.set_trace()
+    if len(matches):
+        matches = ", ".join(matches)
+        return (False, matches)
+    else:
+        return (True, None)
